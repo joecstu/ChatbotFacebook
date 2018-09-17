d66f8edb35c370faabb6d89186a57e2db7d12522
pongsakon <pongsakonpromma@gmail.com>
2018-09-17 14:30:48 +0700

    test

--- app.go
+++ app.go
@@ -89,6 +89,7 @@ func ProcessMessage(event Messaging) {
 			},
 		},
 	}
+	///help meeeee
 	body := new(bytes.Buffer)
 	json.NewEncoder(body).Encode(&response)
 	url := fmt.Sprintf(FACEBOOK_API, os.Getenv("EAAdazaKc9IMBABKolkIaZCcG7pxXhrz0EnlZBVxlaPbZAJG2XmjfC5f49Pk6y2H94B8KYYxZBCsj7f4ApQpdkS1dWyG8EYmY0rEgU9pq9vfsLAVBdP3Q2PDIDQqS5jSgaN4xRuXt9VapF4gYdX3a1FlDUddbEdNB51dKMfEHknMwo6H4OWKH"))
